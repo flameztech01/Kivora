3269478b407b9ca6afa1847fc77e6286f2296f90
flameztech01 <flameztech01@gmail.com>
2026-07-16 00:07:12 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const MONGO_URL = process.env.MONGO_URL;
 app.use(cors({
     origin: [
         'http://localhost:2000',
-        'https://kivora.curriumx.online'
+        'https://kivora.curriumx.online',
+        'http://kivora.curriumx.online'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
